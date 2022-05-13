41f6502d99b7733a3e0aa3be517eedd8b2d1b856
Evgeniy Filipov <filipov62@gmail.com>
2022-05-13 14:21:49 +0300

    Пишем тест для проверки сайта Степик

--- Lesson3_Test_Framework/test_fixture10b.py
+++ Lesson3_Test_Framework/test_fixture10b.py
@@ -15,13 +15,15 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.regression
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
 
-    @pytest.mark.regression
-    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail(reason="fixing this bug right now")
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("input.btn.btn-default")
--- Lesson3_Test_Framework/test_fixture_skip_3_5_4.py
+++ Lesson3_Test_Framework/test_fixture_skip_3_5_4.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
@@ -15,13 +14,11 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.regression
+    @pytest.mark.skip
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
 
-    @pytest.mark.regression
-    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
