39d1d6d2df2d4380c4796a530a632befbaeba655
Evgeniy Filipov <filipov62@gmail.com>
2022-05-16 14:56:14 +0300

    Добавил файл conftest.py:

--- Lesson3_Test_Framework/test_lesson_3_6_3.py
+++ Lesson3_Test_Framework/test_lesson_3_6_3.py
@@ -9,6 +9,7 @@ def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
+    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
 
